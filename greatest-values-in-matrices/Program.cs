a97c8780f7c57f6f5d53bcff01213f1aab27734e
svobop <petr.svoboda@jumpshot.com>
2020-02-26 16:14:51 +0100

    Add project files.

---
+++ greatest-values-in-matrices/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace greatest_values_in_matrices
+{
+    class Program
+    {
+        public static List<List<int>> CompareMatrix(List<List<int>> firstMatrix, List<List<int>> secondMatrix)
+        {
+            var output = firstMatrix;
+            return output;
+        }
+        static void Main(string[] args)
+        {
+            var firstMatrix = new List<List<int>>();
+            firstMatrix.Add(new List<int> { 1, 3 });
+            firstMatrix.Add(new List<int> { 2, 1 });
+
+            var secondMatrix = new List<List<int>>();
+            secondMatrix.Add(new List<int> { -1, 1 });
+            secondMatrix.Add(new List<int> { 2, 2 });
+            
+            Console.WriteLine(CompareMatrix(firstMatrix, secondMatrix));
+        }
+    }
+}
